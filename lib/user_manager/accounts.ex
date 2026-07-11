79f60901b1efc58361a99e7d087cc1d229d1f1c1
AzizBenAmor <azizbenamor1288@gmail.com>
2026-07-11 14:57:44 +0100

    added the repo function for users

---
+++ lib/user_manager/accounts.ex
@@ -0,0 +1,32 @@
+defmodule UserManager.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+  import Ecto.Query, warn: false
+  alias UserManager.Accounts.User
+  alias UserManager.Repo
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_user(user, attrs \\ %{}) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_user(user) do
+    Repo.delete(user)
+  end
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+end
